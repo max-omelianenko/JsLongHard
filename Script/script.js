c8903b35f81b84b2a7d84112408bdead86dac9a8
Maksym Omelianenko <makvitom@gmail.com>
2021-06-03 00:44:04 +0200

    Задание второго урока с оператором **

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,23 @@
 'use strict';
 
-let num = 266219;
+let num, lengthOfNumber, numString, i, result;
 
+num = 266219;
+numString = String(num);
+lengthOfNumber = numString.length;
+i = 0;
+result = 1;
+
+while (i < lengthOfNumber){
+    result *= Number(numString[i]);
+    i++;
+}
+
+console.log('Произведение цифр этого числа: result = ', result);
+result **= 3;
+console.log('Результат в степени 3: result^3 = ', result);
+numString = String(result);
+console.log('Вывести в консоль первые 2 цифры полученного числа: ', numString.substr(0, 2));
 
 
 
