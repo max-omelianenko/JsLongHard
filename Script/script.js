684d14b420f802269bc2edba1397cb8ae74ba184
Maksym Omelianenko <makvitom@gmail.com>
2021-06-05 12:50:34 +0200

    Упорядочил и оптимизировал код, убрал лишние переменные

--- Script/script.js
+++ Script/script.js
@@ -1,23 +1,20 @@
 'use strict';
 
-let num, lengthOfNumber, numString, i, result;
+let num = 266219, 
+i = 0, 
+result = 1, 
+resultPow;
 
-num = 266219;
-numString = String(num);
-lengthOfNumber = numString.length;
-i = 0;
-result = 1;
-
-while (i < lengthOfNumber){
-    result *= Number(numString[i]);
+while (i < String(num).length){
+    result *= Number(String(num)[i]);
     i++;
 }
 
+resultPow = result ** 3;
+
 console.log('Произведение цифр этого числа: result = ', result);
-result **= 3;
-console.log('Результат в степени 3: result^3 = ', result);
-numString = String(result);
-console.log('Вывести в консоль первые 2 цифры полученного числа: ', numString.substr(0, 2));
+console.log('Результат в степени 3: result^3 = ', resultPow);
+console.log('Вывести в консоль первые 2 цифры полученного числа: ', String(resultPow).substr(0, 2));
 
 
 
