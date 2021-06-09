178e43f40e5925729ec521468a03b2e0dfa30d40
Maksym Omelianenko <makvitom@gmail.com>
2021-06-09 17:02:23 +0200

    Исправил задание 3го урока и добавил решение через объект

--- Script/script.js
+++ Script/script.js
@@ -1,18 +1,50 @@
 'use strict';
 
+<<<<<<< Updated upstream
+=======
+//--------------------------------------------------------УРОК 04-------------------------------------------------------
+/*
+let yourString = prompt('Введите свой текст:','    Преимущество концепции «функция-как-объект» заключается в том, что код можно передавать в другую функцию точно так же, как обычную переменную или объект   ');
+
+let lineProcessing = function(a){
+    if (typeof(a) !== 'string'){
+        console.log('Ошибка ввода, повторите попытку');
+    } else if (a.trim().length > 30){
+        console.log(a.trim().substr(0,30) + '...');
+    } else {
+        console.log(a.trim());
+    }
+};
+
+lineProcessing(yourString);
+console.log(typeof(yourString));
+*/
+
+//--------------------------------------------------------УРОК 03-------------------------------------------------------
+
+>>>>>>> Stashed changes
 let lang = prompt('Введите желаемый язык в фотмате "ru" или "en"','ru'),
 daysIf,
 daysSwitch,
+daysElase,
 weekDays = [['ru', ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']], 
 ['en', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']]],
 namePerson = prompt('Назови мне свое имя и я скажу кто ты:', 'Максим'),
 result;
+let weekObject = {
+	ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+	en:  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
 
 weekDays.forEach((row) => {
     if (row[0] === lang) {
         daysIf = row[1];
     }
 });
+
+if (!daysIf) {
+	console.log('IF Неправильно задан язык, вариант', lang, 'некорректный');
+}
 
 switch(lang){
     case 'ru':
@@ -22,7 +54,14 @@ switch(lang){
         daysSwitch = weekDays[1][1];
         break;
     default:
-        (console.log('Неправильно задан язык, вариант', lang, 'некорректный'));
+        (console.log('SWITCH Неправильно задан язык, вариант', lang, 'некорректный'));
+}
+
+const foundRow = weekDays.find(row => row[0] === lang);
+if (foundRow) {
+	daysElase = foundRow[1];
+} else {
+  console.log('Ошибка! Неправильно задан язык, вариант', lang, 'некорректный');
 }
 
 //Второе задание: Определение роли
@@ -31,10 +70,15 @@ console.log('Ты: ', result);
 
 console.log('Дни недели "if":', daysIf);
 console.log('Дни недели "Switch": ', daysSwitch);
+<<<<<<< Updated upstream
 console.log('Дни недели без использования "if" или "switch"', weekDays.find(row => row[0] === lang)[1]);
+=======
+console.log('Дни недели "Object": ', weekObject[lang]);
+console.log('Дни недели без использования "if" или "switch"', daysElase);
+>>>>>>> Stashed changes
 
 
-//Задание второго урока
+//--------------------------------------------------------УРОК 02-------------------------------------------------------
 /*let num = 266219, 
 i = 0, 
 result = 1, 
